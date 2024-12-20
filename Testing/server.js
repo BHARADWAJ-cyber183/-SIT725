592ea21d2a42ee20e6cb959b4384db59d7dd54de
BHARADWAJ-cyber183 <bharatbellamkonda18@gmail.com>
2024-12-20 23:28:53 +1100

    n

--- Testing/server.js
+++ Testing/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const scannerRoutes = require('./routes/scannerRoutes');
 
 const app = express();
-const PORT = 2900;
+const PORT = 2700;
 
 // Middleware to parse JSON and serve static files
 app.use(express.json());
