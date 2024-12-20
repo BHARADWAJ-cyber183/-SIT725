14a7b8f77ed749719b537f1029df510fdec08edc
BHARADWAJ-cyber183 <bharatbellamkonda18@gmail.com>
2024-12-20 21:49:14 +1100

    e

---
+++ Testing/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const scannerRoutes = require('./routes/scannerRoutes');
+
+const app = express();
+const PORT = 8400;
+
+// Middleware to parse JSON and serve static files
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/api', scannerRoutes);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/result', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'result.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
