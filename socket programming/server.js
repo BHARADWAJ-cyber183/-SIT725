ce88f2b17f91ea0fa5f9771e34bbfa6bd5c49bca
BHARADWAJ-cyber183 <bharatbellamkonda18@gmail.com>
2025-01-09 13:40:42 +1100

    r

--- socket programming/server.js
+++ socket programming/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const { Server } = require('socket.io');
 const scannerController = require('./controllers/scannerController');
 
 const app = express();
-const PORT = 9900;
+const PORT = 7700;
+
+// Create an HTTP server for WebSocket integration
+const server = http.createServer(app);
+const io = new Server(server);
 
 // Middleware to parse JSON and serve static files
 app.use(express.json());
@@ -21,7 +27,22 @@ app.get('/result', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'result.html'));
 });
 
+// WebSocket connection for real-time updates
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('start-scan', (data) => {
+    console.log(`Starting scan for URL: ${data.url}`);
+    scannerController.scanWithSocket(data.url, (update) => {
+      socket.emit('scan-update', update);
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
 // Start the server
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
