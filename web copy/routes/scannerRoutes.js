b95f4e21db614f09d69ea45958c5cb34220ca6f0
BHARADWAJ-cyber183 <bharatbellamkonda18@gmail.com>
2025-02-03 23:12:20 +1100

    d

--- web copy/routes/scannerRoutes.js
+++ web copy/routes/scannerRoutes.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const path = require('path');
+const { scanWebsite } = require('../controllers/scannerController');
+
 const router = express.Router();
 
-router.get('/health', (req, res) => {
-    res.json({ status: 'Web Vulnerability Scanner is running.' });
+// Route to serve the home page
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/index.html'));
 });
+
+// Route to serve the result page
+router.get('/result', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/result.html'));
+});
+
+// Route to scan a website
+router.get('/scan', scanWebsite);
 
 module.exports = router;
--- web copy/server.js
+++ web copy/server.js
@@ -3,17 +3,17 @@ const path = require('path');
 const scannerRoutes = require('./routes/scannerRoutes');
 
 const app = express();
-const PORT = 6098;
+const PORT = 5098; // Changed the port to 5098
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views'))); // Serve HTML files correctly
 
 // Routes
 app.use('/', scannerRoutes);
 
-// Start Server
 app.listen(PORT, () => {
   console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
